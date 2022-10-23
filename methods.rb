fe4483cbd07e9da112b1d324fcb421a082ad5374
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-10-23 14:01:20 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,45 @@
 # Your code here!
+# def say_hello()
+#   "hello!"
+# end
+# say_hello()
+
+# def say_hi(name="Rubyist")
+#     puts "Hi there, #{name}!"
+#   end
+  
+#  say_hi "Rhoda"
+
+
+def greet_programmer()
+  puts "Hello, programmer!"
+end
+greet_programmer 
+
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+
+
+def greet_with_default(name="programmer")
+  puts "Hello, #{name}!"
+end
+greet_with_default"sunny"
+
+
+def add(num1,num2)
+  num1 + num2
+end
+add(5,8)
+
+
+
+
+
+  def halve(num)
+    return nil unless num.is_a? Numeric
+    num / 2
+end
